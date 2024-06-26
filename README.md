bfeaf5f4918f25e0ba088b07f7ebfa3a9f645631
Ricky-Case <raca226@uky.edu>
2024-06-26 14:50:33 -0400

    Lesson 160 complete.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # SimpleShooter
 Developed as part of the GameDev.tv "Unreal Engine 5 C++ Developer" course of Udemy.
+
+# Required Assets
+- [Modular SciFi Season 1 Starter Bundle](https://www.unrealengine.com/marketplace/en-US/product/modular-scifi-season-1-starter-bundle)
+- [Paragon: Wraith](https://www.unrealengine.com/marketplace/en-US/product/paragon-wraith)
+
+Asset packs available as an attachment to this [VIDEO](https://www.udemy.com/course/unrealcourse/learn/lecture/31732132#content).
+
+Individual pack downloads available in the UE Marketplace.
---
+++ Source/SimpleShooter/ShooterCharacter.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ShooterCharacter.generated.h"
+
+UCLASS()
+class SIMPLESHOOTER_API AShooterCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AShooterCharacter();
+
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	void MoveForward(float AxisValue);
+	void MoveRight(float AxisValue);
+	void LookUpRate(float AxisValue);
+	void TurnRate(float AxisValue);
+
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float RotationRate = 50.0f;
+};
