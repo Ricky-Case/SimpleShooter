4d01f8a6ea32fe264d2067284175f16e41dbd853
Ricky-Case <raca226@uky.edu>
2024-06-29 10:47:58 -0400

    Lesson 189 complete

---
+++ Source/SimpleShooter/BTTask_ClearBlackboardValue.cpp
@@ -0,0 +1,7 @@
+#include "BTTask_ClearBlackboardValue.h"
+
+/**************************/
+/******PUBLIC METHODS******/
+/**************************/
+
+UBTTask_ClearBlackboardValue::UBTTask_ClearBlackboardValue() { NodeName = TEXT("Clear Blackboard Value"); }
---
+++ Source/SimpleShooter/BTTask_ClearBlackboardValue.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "BTTask_ClearBlackboardValue.generated.h"
+
+UCLASS()
+class SIMPLESHOOTER_API UBTTask_ClearBlackboardValue : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+
+public:
+	UBTTask_ClearBlackboardValue();
+};
